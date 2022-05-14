7465dc0faf9f1969bf009944ebf67caf8ff7c6cc
Izaiah Mendez <mendez.izaiah99@gmail.com>
2022-05-14 01:56:46 -0700

    Add files via upload
    uploaded files

---
+++ main.py
@@ -0,0 +1,25 @@
+
+import pandas as pd 
+
+df = pd.read_csv('product_list.csv')
+
+ps = (df.iloc[3])
+
+print(ps)
+
+
+
+
+
+##greeting function 
+
+##gpu function 
+
+##ps5 function 
+
+##def sony(x,y,z):
+
+
+
+#closing function
+
